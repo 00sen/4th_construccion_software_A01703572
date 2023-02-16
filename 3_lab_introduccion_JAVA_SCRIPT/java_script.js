5ffc9f9d5b030e096ea7f1b0635f3b6251b69d09
sen <inarrituzanetti@outlook.com>
2023-02-16 09:46:39 -0600

    first addition to java_script

---
+++ 3_lab_introduccion_JAVA_SCRIPT/java_script.js
@@ -0,0 +1,17 @@
+var comida = "chilaquiles";
+
+let cena = "tacos";
+
+const precio = 70;
+
+console.log("hola");
+console.info("Valor de la comida: " + comida);
+
+console.warn("El precio no se puede modificar");
+console.error("El precio no se puede modificar");
+
+console.assert(1 === 1);
+console.assert(1 === "1");
+console.assert(1 == "1");
+console.assert(1 == true);
+console.assert(1 === true);
