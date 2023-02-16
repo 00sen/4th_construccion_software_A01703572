042e2c60bf241e789648070800ef91583248106b
sen <inarrituzanetti@outlook.com>
2023-02-16 09:58:44 -0600

    added alerts in java_script

--- 3_lab_introduccion_JAVA_SCRIPT/java_script.js
+++ 3_lab_introduccion_JAVA_SCRIPT/java_script.js
@@ -15,3 +15,23 @@ console.assert(1 === "1");
 console.assert(1 == "1");
 console.assert(1 == true);
 console.assert(1 === true);
+
+// alcance de las variables
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+//Aqui ocurre un erorr porque la variable i ya murio al terminar el ciclo
+//console.log(i);
+
+
+alert("hola!");
+
+let nombre = prompt("Como te llamas?");
+
+console.log("Hola " + nombre);
+
+let is_hungry = confirm("Tienes hambre?");
+
+console.log(is_hungry);
