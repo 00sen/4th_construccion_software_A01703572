66e0f8f5fd733e1a3ad6823c5a6b4c8d614093ec
sen <inarrituzanetti@outlook.com>
2023-02-17 13:39:11 -0600

    first button ready

--- 4_lab_introduccion_JAVA_SCRIPT/js.js
+++ 4_lab_introduccion_JAVA_SCRIPT/js.js
@@ -7,10 +7,56 @@ boton.onclick = () => {
     audio.play();
 }
 
-let boton_2 = document.getElementById("boton_1");
+//actividad 1 ----------------------------------------
+
+let boton_2 = document.getElementById("boton_2");
 boton_2.innerHTML = "Click para iniciar ejercicio 1";
 boton_2.onclick = () => {
+    let texto = document.getElementById("resultado1");
     let num = prompt("Ingrese numero para prueba");
-    num = parseInt(num);
-    console.log(num);
+    let num1 = parseInt(num);
+    if(num1 <= 0){
+        alert("Numero debe ser igual o mayor a 1");
+    } else if(num1 == 1){
+        texto.innerHTML = "1<br>1<br>1";
+    } else {
+        let tabla = "<table>";
+        for(let i = 1; i<= 3; i++){
+            tabla = tabla + "<tr>";
+            if(i == 1){
+                for(let i = 1; i <=num1; i++){
+                    tabla = tabla + "<th>";
+                    tabla = tabla + i;
+                    tabla = tabla + "</th>";
+                }
+            } else if(i == 2){
+                for(let i = 1; i <=num1; i++){
+                    tabla = tabla + "<th>";
+                    tabla = tabla + i * i;
+                    tabla = tabla + "</th>";
+                }
+            } else if(i == 3){
+                for(let i = 1; i <=num1; i++){
+                    tabla = tabla + "<th>";
+                    tabla = tabla + i*i*i;
+                    tabla = tabla + "</th>";
+                }
+            }
+            tabla = tabla + "</tr>";
+        }
+        tabla = tabla + "</table>";
+        texto.innerHTML = tabla;
+    }
+}
+
+//actividad 2 ---------------------------------------------
+
+let boton_3 = document.getElementById("boton_3");
+boton_3.innerHTML = "Click para iniciar ejercicio 2";
+boton_3.onclick = () => {
+    let texto = document.getElementById("resultado2");
+    let num1 = Math.floor(Math.random() * 100);
+    let num2 = Math.floor(Math.random() * 100);
+    let resultado = prompt("Ingrese resultado");
+
 }
