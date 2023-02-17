2b0cebc232d9d98d6c630254ce2ba701356e82f2
sen <inarrituzanetti@outlook.com>
2023-02-17 11:59:18 -0600

    deleted extra 5th directory

--- 4_lab_introduccion_JAVA_SCRIPT/js.js
+++ 4_lab_introduccion_JAVA_SCRIPT/js.js
@@ -6,3 +6,11 @@ boton.onclick = () => {
     let audio = document.getElementById("audio");
     audio.play();
 }
+
+let boton_2 = document.getElementById("boton_1");
+boton_2.innerHTML = "Click para iniciar ejercicio 1";
+boton_2.onclick = () => {
+    let num = prompt("Ingrese numero para prueba");
+    num = parseInt(num);
+    console.log(num);
+}
