1e70448815a55d3c268e16eabaef4acbe6dfa0fb
sen <inarrituzanetti@outlook.com>
2023-02-16 21:35:39 -0600

    corrected lab 2-3 css

---
+++ 4_lab_introduccion_JAVA_SCRIPT/js.js
@@ -0,0 +1,8 @@
+//java script lab 3
+
+let boton = document.getElementById("boton_audio");
+boton.innerHTML = "Click para iniciar musica epica";
+boton.onclick = () => {
+    let audio = document.getElementById("audio");
+    audio.play();
+}
