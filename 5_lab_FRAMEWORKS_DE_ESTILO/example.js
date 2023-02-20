d0965f56b922994d6c7929c6e54e030339dea146
sen <inarrituzanetti@outlook.com>
2023-02-20 11:43:39 -0600

    just added lab5 dir

---
+++ 5_lab_FRAMEWORKS_DE_ESTILO/example.js
@@ -0,0 +1,104 @@
+var comida = "chilaquiles";
+
+let cena = "tacos";
+
+const precio = 70;
+
+console.log("hola");
+console.info("Valor de la comida: " + comida);
+
+console.warn("El precio no se puede modificar");
+console.error("El precio no se puede modificar");
+
+console.assert(1 === 1);
+console.assert(1 === "1");
+console.assert(1 == "1");
+console.assert(1 == true);
+console.assert(1 === true);
+
+// alcance de las variables
+
+for(let i = 1; i <= 10; i++){
+    console.log(i);
+}
+
+//Aqui ocurre un erorr porque la variable i ya murio al terminar el ciclo
+//console.log(i);
+
+
+alert("hola!");
+
+let nombre = prompt("Como te llamas?");
+
+console.log("Hola " + nombre);
+
+let is_hungry = confirm("Tienes hambre?");
+
+console.log(is_hungry);
+
+
+//funciones tradicionales
+
+function numero_tacos(){
+    return 5;
+}
+
+console.log(numero_tacos());
+
+
+//funciones modernas / anonimas / flecha / arrow function
+
+let cantidad_tacos = () => {return 5;}
+
+console.log(cantidad_tacos());
+
+let boton = document.getElementById("buenos dias");
+boton.innerHTML = "Buenos dias!";
+boton.onclick = () => alert("Buenos diaaas");
+
+
+
+//arreglos
+const arreglo = ["Elemento"];
+arreglo.push("Otro elemento");
+arreglo["dos"] = 2;
+arreglo.length = 10;
+arreglo[20] = "Fin del arreglo";
+
+console.log(arreglo);
+
+for (let elemento in arreglo) {
+    console.log(elemento);
+}
+
+for (let valor of arreglo) {
+    console.log(valor);
+}
+
+
+
+//objetos
+
+const objeto = {
+    atributo_1: "valor 1",
+    atributo_2: 2
+}
+
+console.log(objeto);
+
+
+
+console.log(objeto.atributo_1);
+console.log(objeto.atributo_2);
+objeto.atributo_3 = "tres";
+
+
+for(let elemento in objeto){
+    console.log(elemento);
+}
+
+
+//arreglo de objeto
+
+const arreglo_de_objeto = [{atributo_1: "valor 0", atributo_2: 1},{}, objeto];
+console.log(arreglo_de_objeto);
