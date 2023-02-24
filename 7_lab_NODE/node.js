29d6bd79399cacfefa9511b066234122b66687dd
sen <inarrituzaneti@outlook.com>
2023-02-24 12:59:46 -0600

    lab 8 TERMINADO

--- 7_lab_NODE/node.js
+++ 7_lab_NODE/node.js
@@ -1,33 +1,88 @@
-console.log("hola mundo desde node");
-console.info("Este script se está ejecutando desde una computadora y no desde el navegador");
-console.warn("document, alert, confirm y prompt, no existen en el entorno de ejecución de node");
-console.error("Así se ve un error");
-
-//fs es el módulo para trabajar con el sistema de archivos
-const filesystem = require('fs');
-filesystem.writeFileSync('hola.txt', 'Hola desde node');
-console.log("Terminamos de escribir el archivo");
-
-
-const arreglo = [7000, 5000, 60, 90, 100, 10, 20, 10000, 0, 120, 2000, 340, 1000, 50];
-
-for (let item of arreglo) {
-    setTimeout(() => {console.log(item);}, item);
-} 
-
-console.log("Este log está después de imprimir el arreglo");
-
-setTimeout(() => {console.log("Ya te hackié");}, 12000);
-
-//http es el módulo que permite crear un servidor que pueda atender peticiones http
+const fileSystem = require('fs');
 const http = require('http');
 
-const server = http.createServer( (request, response) => {    
-    console.log(request.url);
-    response.setHeader('Content-Type', 'text/html');
-    response.write("<h1>Chilaquiles</h1>");
-    response.write("Hola desde node!");
-    response.end();
+
+const data = fileSystem.readFileSync('datos.txt', 'utf8');
+const numeros_string = data.split('\n');
+const numeros = [];
+for(let numero of numeros_string){
+    numeros.push(Number(numero));
+}
+
+function sacar_promedio(arreglo){
+    let promedio = 0;
+    for(let i = 0; i<arreglo.length; i++){
+        promedio = promedio + arreglo[i];
+    }
+    promedio = promedio / arreglo.length;
+    return promedio;
+}
+
+let intro = 'Datos del archivo datos.txt<br>';
+let primer_texto = "";
+
+for (let i = 0; i < numeros_string.length; i++){
+    primer_texto = primer_texto + numeros_string[i];
+    primer_texto = primer_texto + " | ";
+}
+
+
+let promedio = sacar_promedio(numeros);
+let promedio_texto = "<br><br> Promedio: ";
+promedio_texto += promedio;
+
+let string_para_archivo = "'texto de prueba'";
+
+function escribir_archivo(texto){
+    fileSystem.writeFileSync('texto.txt', texto);
+}
+
+escribir_archivo(string_para_archivo);
+
+let num = Math.floor(Math.random() * 500);
+
+function EsPrimo(esto){
+    if(esto == 0 || esto == 1 || esto == 4) return false;
+    for(let i = 2; i < esto / 2; i++){
+        if(esto % i == 0)return false;
+    }
+    return true;
+}
+
+let bool = EsPrimo(num);
+
+let comprobador = "";
+if(bool == false){
+    comprobador = " NO es primo.";
+} else{
+    comprobador = " SI es primo."
+}
+let resultado3 = "El numero " + num
++ comprobador;
+
+
+fileSystem.readFile("./NODE.html", 'utf-8', function (err, htmlData){
+
+    const server = http.createServer( (request, response) => {
+        console.log(request.url);
+        response.setHeader('Content-Type', 'text/html');
+        response.write(intro);
+        response.write(primer_texto);
+        response.write(promedio_texto + "<br><br>");
+        response.write("Texto para escribir en el archivo 'texto.txt' : <br><br>");
+        response.write(string_para_archivo);
+        response.write("<br><br> El problema opcional es saber<br>si un numero es primo o no.");
+        response.write("<br><br>Numero prueba aleatorio: "+ num + "<br><br>");
+        response.write(resultado3+"<br><br>");
+        response.write("---------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+        response.write("<br><br><br> Pagina Web");
+        response.write(htmlData);
+    
+    
+        response.end();
+    });
+    
+    server.listen(3000);
+
 });
 
-server.listen(3000);
--- README.md
+++ README.md
@@ -1,3 +1,1 @@
 # 4th_construccion_software_A01703572
-
-prueba ubuntu virtual box
